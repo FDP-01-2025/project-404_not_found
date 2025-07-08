2f423f57729c979e207a01b6a5533ad0b9d16a66
mnunez-071 <00070822@uca.edu.sv>
2025-07-07 21:08:21 -0600

    00070822

--- src/functions.cpp
+++ src/functions.cpp
@@ -20,3 +20,42 @@ const char BLOCK_MOVING = 219;    // Carácter ASCII para bloque en movimiento (
 const char BLOCK_FIXED = 176;     // Carácter ASCII para bloque fijo (░)
 const char EMPTY_SPACE = ' ';     // Espacio vacío
 const char GAMEOVER_MARKER = '.'; // Marcador visual para línea de game over
+
+/**
+ * FORMAS DE LAS PIEZAS:
+ * - 4 piezas diferentes (I, O, T, L)
+ * - Cada pieza tiene 4 rotaciones posibles
+ * - Cada rotación es una matriz 4x4 donde 1 = bloque y 0 = espacio vacío
+ */
+
+int shapes[4][4][4][4] = {
+    // Pieza I (forma de palo)
+    {
+        {{0,0,0,0}, {1,1,1,1}, {0,0,0,0}, {0,0,0,0}}, // Horizontal
+        {{0,0,1,0}, {0,0,1,0}, {0,0,1,0}, {0,0,1,0}}, // Vertical
+        {{0,0,0,0}, {0,0,0,0}, {1,1,1,1}, {0,0,0,0}}, // Horizontal (alternativa)
+        {{0,1,0,0}, {0,1,0,0}, {0,1,0,0}, {0,1,0,0}}  // Vertical (alternativa)
+    },
+    // Pieza O (cuadrado)
+    {
+        {{0,0,0,0}, {0,1,1,0}, {0,1,1,0}, {0,0,0,0}}, // Todas las rotaciones iguales
+        {{0,0,0,0}, {0,1,1,0}, {0,1,1,0}, {0,0,0,0}},
+        {{0,0,0,0}, {0,1,1,0}, {0,1,1,0}, {0,0,0,0}},
+        {{0,0,0,0}, {0,1,1,0}, {0,1,1,0}, {0,0,0,0}}
+    },
+    // Pieza T
+    {
+        {{0,0,0,0}, {0,1,0,0}, {1,1,1,0}, {0,0,0,0}}, // T normal
+        {{0,0,0,0}, {0,1,0,0}, {0,1,1,0}, {0,1,0,0}}, // Rotada 90°
+        {{0,0,0,0}, {0,0,0,0}, {1,1,1,0}, {0,1,0,0}}, // Rotada 180°
+        {{0,0,0,0}, {0,1,0,0}, {1,1,0,0}, {0,1,0,0}}  // Rotada 270°
+    },
+    // Pieza L
+    {
+        {{0,0,0,0}, {1,0,0,0}, {1,1,1,0}, {0,0,0,0}}, // L normal
+        {{0,0,0,0}, {0,1,1,0}, {0,1,0,0}, {0,1,0,0}}, // Rotada 90°
+        {{0,0,0,0}, {0,0,0,0}, {1,1,1,0}, {0,0,1,0}}, // Rotada 180°
+        {{0,0,0,0}, {0,1,0,0}, {0,1,0,0}, {1,1,0,0}}  // Rotada 270°
+    }
+};
+
